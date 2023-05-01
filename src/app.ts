b6992b65be0e57031668c6a6f6b1883a0926978f
kazuki nakama <kazuki_nakama@rebuild-inc.co.jp>
2023-05-01 19:00:38 +0900

    DB接続確認

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
+import { pool } from 'database/connection';
 import express, { Application, json, Request, Response } from 'express';
 
 export const app: Application = express();
 
 app.use(json());
+
+pool
+  .getConnection()
+  .then((connection) => console.log('connected as id ' + connection.threadId))
+  .catch((error) => console.error(error.stack));
 
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Hello World!' });
